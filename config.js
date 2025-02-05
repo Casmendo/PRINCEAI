5022386640a401c5112ed65c4855bf9a42310464
Casmendo <isahmusa9921@gmail.com>
2025-02-05 21:39:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -161,7 +161,7 @@ global.eror = '```404 error```'
 
 dotenv.config()
 
-const ownervb = process.env.2349039951951,RMS";
+const ownervb = process.env. 2349039951951,RMS";
 if (!ownervb){
    throw new Error("OWNER_NUMBER var env is not set please set it e.g 2349039951951,RMS");
 }
